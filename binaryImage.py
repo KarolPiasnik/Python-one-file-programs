ffdf662bb4864a1a69ed29966faf605cdc6174f4
Karol Piaśnik <piasnik.karol@outlook.com>
2016-12-09 08:44:06 +0100

    binaryImage: shell version deleted, starting real bitmap drawing

--- binaryImage.py
+++ binaryImage.py
@@ -9,6 +9,7 @@ clock = pygame.time.Clock()
 
 black = (0,0,0)
 white = (255,255,255)
+count = 0
 
 def mainLoop():
     while(True):
@@ -20,19 +21,5 @@ def mainLoop():
 
 
     
-imgIn = input("Enter your bitmap image")
-imgOut = ""
-count = 0
-for char in imgIn:
-    if count % 6 == 0:
-        imgOut += '\n'
-    count+=1
 
-    if char == '1':
-        imgOut += '*'
-    else:
-        imgOut+= ' '
-        
-    
-print(imgOut)
 mainLoop()
