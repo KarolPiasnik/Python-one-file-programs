5e7b690f3f4fe2a8548592739f0469e2054322cc
Karol Piaśnik <piasnik.karol@outlook.com>
2016-12-12 08:15:02 +0100

    binaryImage: now it will read binary version of bitmap from textfile

--- binaryImage.py
+++ binaryImage.py
@@ -13,40 +13,10 @@ black = (0,0,0)
 white = (255,255,255)
 imageDisplay.fill(white)
 
+content = open('image.txt', 'r').read()
 
-def mainLoop():
-    count = 0 #pixel counter
-    size = 10
-
-    while(True):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_0:
-                    for i in range(size):
-                        imageDisplay.set_at((count%width, count//height), black)
-                        count+=1
-                elif event.key == pygame.K_1:
-                    for i in range(size):
-                        imageDisplay.set_at((count%width, count//height), white)
-                        count+=1
-
-        keys = pygame.key.get_pressed()  #checking pressed keys
-        if keys[pygame.K_0]:
-            for i in range(size):
-                imageDisplay.set_at((count%width, count//height), black)
-                count+=1
-        if keys[pygame.K_1]:
-            for i in range(size):
-                imageDisplay.set_at((count%width, count//height), white)
-                count+=1
-
-        pygame.display.update()
-        clock.tick(30)
 
 
 
     
 
-mainLoop()
