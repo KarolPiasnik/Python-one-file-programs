e52c881a9206083088198c22474c8470a4a6b496
Karol Piaśnik <piasnik.karol@outlook.com>
2016-12-12 17:39:33 +0100

    binaryImage: color mode not working, printing only first square

--- binaryImage.py
+++ binaryImage.py
@@ -11,6 +11,13 @@ pygame.init()
 size = 10 # size of units to be drawn
 black = (0,0,0)
 white = (255,255,255)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+some = (255,255,0)
+some2 = (255,0,255)
+some3 = (0,255,255)
+colors = [black,white,red,green,blue,some,some2,some3]
 imageDisplay.fill((155,155,155))
 content = open('image.txt', 'r').read()
 
@@ -24,6 +31,19 @@ def makeImageWB(): #print image white and black
             pygame.draw.rect(imageDisplay,black,(count%(width//size)*size, count//(width//size)*size, size, size))
         count += 1
 
+def makeImage8Color(): #print image white and black
+    color = 0
+    count = 0 # needed to calculate where to draw
+    for char in content[1:]:
+        if count % 3 == 2:
+                count -= 2
+                pygame.draw.rect(imageDisplay,colors[color],(count%(width//size)*size, count//(width//size)*size, size, size))
+                color = 0
+        else:
+            color += 2**(count%3)
+            
+        count += 1
+
 def introLoop():
     while(True):
         for event in pygame.event.get(): #event handling loop
@@ -31,7 +51,9 @@ def introLoop():
                     pygame.quit()
         pygame.display.update()
         clock.tick(10)
-if content[0] == '1':                  
+if content[0] == '0':                  
     makeImageWB()
+else:
+    makeImage8Color()
 introLoop()    
 
